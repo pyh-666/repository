d66ffe2bc3a793e100396cd95b11d8d407caca6e
HYX-1999 <1502318069@qq.com>
2020-09-09 19:59:17 +0800

    主干添加

--- hellogit/src/main/java/com/atguigu/git/hellogit.java
+++ hellogit/src/main/java/com/atguigu/git/hellogit.java
@@ -5,5 +5,6 @@ public class hellogit {
         System.out.println("hellogit");
         System.out.println("更新1");
         System.out.println("创建分支");
+        System.out.println("主干添加");
     }
 }
